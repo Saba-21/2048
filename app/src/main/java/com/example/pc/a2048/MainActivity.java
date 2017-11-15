72c85e6513815726cb47bcd3006149ab4bded1e0
Saba-21 <stoningrolle@gmail.com>
2017-11-15 21:13:33 +0400

    board created

--- app/src/main/java/com/example/pc/a2048/MainActivity.java
+++ app/src/main/java/com/example/pc/a2048/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.pc.a2048;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -14,9 +13,13 @@ import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView orientation;
+    private String orientation;
     private RelativeLayout main_view;
     private float startX, endX, startY, endY;
+    private final int allBoard = 16;
+    private TextView[] boardHolder = new TextView[allBoard];
+    private String[] boardValues;
+    private int[] boardIDs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
         initView();
 
+        setValuesToBoard();
+
+        getTouchOrientation();
+
+    }
+
+    private void getTouchOrientation(){
         main_view.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
@@ -43,29 +54,36 @@ public class MainActivity extends AppCompatActivity {
                         endX = event.getRawX() - v.getX();
                         endY = event.getRawY() - v.getY();
 
-                        if(endX-startX > 0 && endX - startX > endY - startY)
-                            orientation.setText("Right");
-                        if(endY - startY > 0 && endX - startX < endY - startY)
-                            orientation.setText("Bottom");
-                        if(endX-startX < 0 && abs(endX - startX) > abs(endY - startY))
-                            orientation.setText("Left");
-                        if(endY - startY < 0 && abs(endX - startX) < abs(endY - startY))
-                            orientation.setText("Top");
+                        if (endX - startX > 0 && endX - startX > endY - startY)
+                            orientation="Right";
+                        if (endY - startY > 0 && endX - startX < endY - startY)
+                            orientation="Bottom";
+                        if (endX - startX < 0 && abs(endX - startX) > abs(endY - startY))
+                            orientation="Left";
+                        if (endY - startY < 0 && abs(endX - startX) < abs(endY - startY))
+                            orientation="Top";
+
+                        Toast.makeText(MainActivity.this, orientation, Toast.LENGTH_SHORT).show();
 
                         break;
                 }
                 return true;
             }
         });
+    }
 
-
-
-
-
+    private void setValuesToBoard(){
+        for(int i = 0; i<allBoard; i++)
+            boardHolder[i].setText(boardValues[i]);
     }
 
     private void initView() {
-        orientation = findViewById(R.id.orientation);
         main_view = findViewById(R.id.main_view);
+        boardValues = getResources().getStringArray(R.array.board);
+        boardIDs = new int[]{R.id.board_1,R.id.board_2,R.id.board_3,R.id.board_4,R.id.board_5,
+                R.id.board_6,R.id.board_7,R.id.board_8,R.id.board_9,R.id.board_10,R.id.board_12,
+                R.id.board_11,R.id.board_13,R.id.board_14,R.id.board_15,R.id.board_16};
+        for(int i = 0; i<allBoard; i++)
+            boardHolder[i] = findViewById(boardIDs[i]);
     }
 }
